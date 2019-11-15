ad2acd719936349e3a1944a595c380bc821c1979
Yann Bai (yannbo) <yannbo@cisco.com>
2019-11-15 09:30:30 +0800

    add comment

--- src/main/java/com/springboot/demo/dao/UserDao.java
+++ src/main/java/com/springboot/demo/dao/UserDao.java
@@ -7,6 +7,13 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
-public interface UserDao extends PagingAndSortingRepository<User, Long>,JpaSpecificationExecutor<User>,JpaRepository<User,Long> {
+public interface UserDao extends PagingAndSortingRepository<User, Long>, JpaSpecificationExecutor<User>, JpaRepository<User, Long> {
+    /**
+     * The superclass method <code>Optional<User> findById(Long var1);</code> does not meet the query type,
+     * so add a method to query user by string id.
+     *
+     * @param id
+     * @return
+     */
     User findById(String id);
 }
